d1f84e0245aee01e1a9c14fcf8412d78cfad72f5
Ashton Hotke <339741886@GoTVDSB.ca>
2024-10-08 08:44:28 -0400

    Add project files.

---
+++ Assignment 8 - Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment_8___Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading in colours
+
+            List <string> colours = new List<string> { "Red", "Yellow", "Violet", "Green", "Orange" };
+            for (int i = 0; i < colours.Count; i++)
+            {
+                Console.WriteLine(colours[i]);
+            }
+
+            // Random Numbers
+
+            int numbersAmount, min, max;
+            Console.WriteLine("Please enter how many numbers you need:");
+            numbersAmount = Int32(Console.ReadLine());
+            List<int> numbers = new List<int> {  };
+        }
+    }
+}
